e9f5cf5ad776998e87af9c26e048ebccb9e5d87d
Kevin Han <yd.han181@gmail.com>
2019-07-31 20:25:20 -0400

    Added PUT and DELETE /v1/exercises

--- routes/exercises.js
+++ routes/exercises.js
@@ -17,4 +17,17 @@ router.post('/', (req, res, next) => {
         .catch(next)
 })
 
+router.put('/:id', (req, res, next) => {
+    Exercise.findByPk(req.params.id)
+        .then(exercise => exercise.update(req.body))
+        .then(exercise => res.json(exercise))
+        .catch(next)
+})
+
+router.delete('/:id', (req, res, next) => {
+    Exercise.destroy({ where: { id: req.params.id } })
+        .then(() => res.json({ msg: 'Exercise successfully deleted.' }))
+        .catch(next)
+})
+
 module.exports = router
